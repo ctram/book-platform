48486740ace0706bd8d8a606d2df83cbe0bd628d
Chris Tram <chris.tram@gmail.com>
2018-01-12 21:25:13 -0800

    express, jquery, bootstrap,

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', (req, res) => {
+                     res.send('Hello World');
+});
+
+app.listen(port, err => {
+});

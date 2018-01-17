4eb781be052b527c03a01b4760447081b2088d5e
Chris Tram <chris.tram@gmail.com>
2018-01-16 20:12:07 -0800

    add more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,45 @@
-let gulp = require('gulp');
-gulp.task();
+const gulp = require('gulp');
+// const jshint = require('gulp-jshint');
+const jsFiles = ['*.js', 'src/**/*.js'];
+const jscs = require('gulp-jscs');
+const nodemon = require('nodemon');
+
+gulp.task('style', () => {
+  return gulp
+    .src(jsFiles)
+    // .pipe(jshint())
+    // .pipe(
+    //   jshint.reporter('jshint-stylish', {
+    //     verbose: true
+    //   })
+    // )
+    .pipe(jscs());
+});
+
+gulp.task('inject', () => {
+  const wiredep = require('wiredep').stream;
+  const options = {
+    bowerJson: require('./bower.json'),
+    directory: './public/lib'
+  };
+
+  return gulp
+    .src('./src/views/*html.')
+    .pipe(wiredep(options))
+    .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', () => {
+  const options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      PORT: 3000
+    },
+    watch: jsFiles
+  };
+
+  return nodemon(options).on('restart', ev => {
+    console.log('Restarting...');
+  });
+});
